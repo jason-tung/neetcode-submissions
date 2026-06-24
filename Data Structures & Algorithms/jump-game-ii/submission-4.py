392beefdb9753565df7568126771c8a256553597
Jason Tung <jt568@cornell.edu>
2026-06-23 19:51:15 -0700

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        d = [float('inf')] * len(nums)
+        d[-1] = 0
+        def dfs(i):
+            if i == len(nums) - 1:
+                return 0
+            r = min(i+nums[i] + 1, len(nums))
+            for j in range(i + 1, r):
+                if j < len(nums) and d[j] == float('inf'):
+                    dfs(j)
+            d[i] =(min(d[i+1:r]) if len(d[i+1:r]) else float('inf')) + 1
+            return d[i]
+        return dfs(0)
