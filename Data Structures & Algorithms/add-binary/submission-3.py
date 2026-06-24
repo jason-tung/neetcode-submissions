4491bd12aa8ac55f99565cc9bb4ec77021f7ce4c
Jason Tung <jt568@cornell.edu>
2026-06-24 16:25:53 -0700

    Add: add-binary - submission-3

---
+++ Data Structures & Algorithms/add-binary/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        co = 0
+        for i in range(1,max(len(a),len(b))+1):
+            ad = bd = 0
+            if i <= len(a):
+                ad = int(a[-i])
+            if i <= len(b):
+                bd = int(b[-i])
+            local_sum = ad + bd + co
+            co = local_sum >> 1
+            dig = local_sum & 1
+            res.append(str(dig))
+        if co:
+            res.append(str(co))
+        return ''.join(res[::-1])
+                
