cb65d726091c360e5b35df4bb72016be1939ea4a
Jason Tung <jt568@cornell.edu>
2026-06-26 03:09:27 -0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = {}
+        a[1],a[2] = 1,2
+        for i in range(3, n + 1):
+            a[i] = a[i-1] + a[i-2]
+        return a[n]
