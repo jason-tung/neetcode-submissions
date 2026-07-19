a899eda762853c4ed130470a21472ba296003047
Jason Tung <jt568@cornell.edu>
2026-07-19 13:08:30 -0700

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.py
@@ -0,0 +1,29 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.end = False
+        self.children = {}
+
+    def addWord(self, word: str) -> None:
+        def add(node, i):
+            if i == len(word):
+                node.end = True
+            else:
+                if word[i] not in node.children:
+                    node.children[word[i]] = WordDictionary()
+                add(node.children[word[i]], i + 1)
+        add(self, 0)
+
+    def search(self, word: str) -> bool:
+        def seek(node, i):
+            if i == len(word):
+                return node.end
+            c = word[i]
+            if c != '.':
+                if c not in node.children:
+                    return False
+                return seek(node.children[c], i + 1)
+            else:
+                return any((seek(node.children[k], i + 1) for k in node.children))
+        return seek(self, 0)
+        
