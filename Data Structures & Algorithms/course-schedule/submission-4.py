dceae873f172c56871ee859eed52a47a2c6e4ca9
Jason Tung <jt568@cornell.edu>
2026-07-30 20:42:09 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = {}
+        solved = set()
+        for k in range(numCourses):
+            d[k] = set()
+        for (a,b) in prerequisites:
+            d[a].add(b)
+        def dfs(n):
+            for k in d[n]:
+                if k not in solved:
+                    if k in visited:
+                        return False
+                    visited.add(k)
+                    if not dfs(k):
+                        return False
+            solved.add(n)
+            return True
+        for n in range(numCourses):
+            if n not in solved:
+                visited = set([n])
+                if not dfs(n):
+                    return False
+        return True
