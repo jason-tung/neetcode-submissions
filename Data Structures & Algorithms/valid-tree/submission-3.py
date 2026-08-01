af36b0a553438926aa66e62e65cc1e60dd72fc1c
Jason Tung <jt568@cornell.edu>
2026-07-31 17:52:39 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        d = [[] for _ in range(n)]
+        for (a,b) in edges:
+            d[a].append(b)
+            d[b].append(a)
+        solved = [False for _ in range(n)]
+        visited = [False for _ in range(n)]
+        def dfs(n, prev):
+            for k in d[n]:
+                if k != prev and not solved[n]:
+                    if visited[k]:
+                        return False 
+                    visited[k] = True
+                    if not dfs(k, n):
+                        return False
+                    visited[k] = False
+            solved[n] = True
+            return True
+        dfs(0, -1)
+        return all(solved)
+            
