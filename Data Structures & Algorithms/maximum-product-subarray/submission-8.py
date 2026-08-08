f63976724364bd5e26b7f5337bae3b0d45e5d764
Jason Tung <jt568@cornell.edu>
2026-08-07 19:43:27 -0700

    Add: maximum-product-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        dp = [-math.inf] * len(nums)
+        dpn = [math.inf] * len(nums)
+        dp[0] = nums[0]
+        if nums[0] < 0:
+            dpn[0] = nums[0] 
+        m = dp[0]
+        for i in range(1, len(nums)):
+            if nums[i] >= 0:
+                dpn[i] = nums[i] * dpn[i-1]
+                dp[i] = max(nums[i] * dp[i-1], nums[i])
+            else:
+                dpn[i] = min(nums[i], dp[i-1] * nums[i])
+                dp[i] = max(nums[i], dpn[i-1] * nums[i])
+            m = max(m, dp[i])
+        return m
