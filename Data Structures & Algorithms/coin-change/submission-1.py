0c45ad4054e1db555f2990e176f2165f9a924210
Jason Tung <jt568@cornell.edu>
2026-08-07 18:30:25 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # opt(i+k) = opt(i) + 1 for all k in coins
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
+        for i in range(amount + 1):
+            if dp[i] != -1: 
+                for k in coins:
+                    if i + k <= amount:
+                        if dp[i+k] == -1:
+                            dp[i+k] = dp[i] + 1
+                        else:
+                            dp[i+k] = min(dp[i+k], dp[i] + 1)
+        return dp[-1]
