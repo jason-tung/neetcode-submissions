5c97a5ad755a4f1621bc3ce5c1dbffc516738a26
Jason Tung <jt568@cornell.edu>
2026-08-19 19:43:17 -0700

    Add: longest-increasing-path-in-matrix - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-1.py
@@ -0,0 +1,22 @@
+d = [(0,1), (1,0), (0,-1), (-1,0)]
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n,m = len(matrix), len(matrix[0])
+        dp=[[-1]*m for _ in range(n)]
+        mx = 1
+        def dfs(i,j):
+            nonlocal mx
+            if dp[i][j] == -1:
+                tot = 1
+                for (r,c) in d:
+                    ni, nj = i + r, j + c
+                    if 0 <= ni < n and 0 <= nj < m and matrix[ni][nj] > matrix[i][j]:
+                        tot = max(tot, dfs(ni, nj) + 1)
+                dp[i][j] = tot
+                mx = max(mx, tot)
+            return dp[i][j]
+        for i in range(n):
+            for j in range(m):
+                dfs(i,j)
+        return mx
