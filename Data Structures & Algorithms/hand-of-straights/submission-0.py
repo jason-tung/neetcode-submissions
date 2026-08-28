b9483a17b625c15226e0c01f4faabeb9d4cb02af
Jason Tung <jt568@cornell.edu>
2026-08-27 17:00:17 -0700

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,14 @@
+import bisect
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        d = Counter(hand)
+        for k in hand:
+            if d[k] != 0:
+                d[k] -= 1
+                for i in range(1,groupSize):
+                    t = k + i
+                    if t not in d or d[t] == 0:
+                        return False
+                    d[t] -= 1
+        return True
