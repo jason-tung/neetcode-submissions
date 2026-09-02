ff3f69f10f70be009fa0e77792a75d5d17a6f78e
Jason Tung <jt568@cornell.edu>
2026-09-02 14:01:21 -0700

    Add: insert-new-interval - submission-8

---
+++ Data Structures & Algorithms/insert-new-interval/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], tar: List[int]) -> List[List[int]]:
+        res = []
+        i = 0
+        while i < len(intervals) and intervals[i][1] < tar[0]:
+            res.append(intervals[i])
+            i += 1
+        while i < len(intervals) and tar[1] >= intervals[i][0]:
+            tar = [min(tar[0], intervals[i][0]), max(tar[1], intervals[i][1])]
+            i += 1
+        res.append(tar)
+        for j in range(i, len(intervals)):
+            res.append(intervals[j])
+        return res
