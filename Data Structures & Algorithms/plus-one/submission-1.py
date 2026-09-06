2c32dcab316671628ca041b9e5e2bc9d7ca5e56b
Jason Tung <jt568@cornell.edu>
2026-09-06 16:24:04 -0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        co = 0
+        digits[-1] += 1
+        for i in range(len(digits) - 1, -1, -1):
+            s = co + digits[i]
+            print(i,s)
+            digits[i] = s % 10
+            co = s // 10
+        return [1] + digits if co else digits
