16c55d32a0daf44c024e09c6ae3e48b952ace1bc
Jason Tung <jt568@cornell.edu>
2026-09-11 17:56:34 -0700

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        d = defaultdict(list)
+        done = set()
+        mapper = {}
+        for u,v,t in times:
+            d[u].append((t,v))
+        q = [(0,k)]
+        cost = 0
+        while q and len(done) != n:
+            pu, u = heapq.heappop(q)
+            if u in done:
+                continue
+            done.add(u)
+            cost = max(pu, cost)
+            for (pv, v) in d[u]:
+                pt = pv + pu
+                if v not in mapper or mapper[v] > pt:
+                    mapper[v] = pt
+                    heapq.heappush(q, (pt, v))
+        return cost if len(done) == n else -1
+        
+    
+# node -> (price, output)
+
+# pop top of minhead
+# for each d[u]
+# enqueue onto minheap as (total price, outputnode)
