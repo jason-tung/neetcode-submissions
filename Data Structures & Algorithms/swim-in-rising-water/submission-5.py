88825bd52b47645fdb07ccaf5e7b13ad2a8eea80
Jason Tung <jt568@cornell.edu>
2026-09-15 17:26:01 -0700

    Add: swim-in-rising-water - submission-5

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-5.py
@@ -0,0 +1,15 @@
+dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        seen = [[False] * n for _ in range(n)]
+        q = [(grid[0][0], 0, 0)]
+        while q:
+            cost, i, j = heapq.heappop(q)
+            if i == j == n - 1:
+                return cost
+            for di, dj in dirs:
+                ip, jp = i + di, j + dj
+                if 0 <= ip < n and 0 <= jp < n and not seen[ip][jp]:
+                    seen[ip][jp] = True
+                    heapq.heappush(q, (max(cost, grid[ip][jp]), ip, jp))
