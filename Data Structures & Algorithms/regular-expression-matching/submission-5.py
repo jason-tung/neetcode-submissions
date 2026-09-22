25748e21cb67c27d0de5139784d994704f951d99
Jason Tung <jt568@cornell.edu>
2026-09-21 19:22:35 -0700

    Add: regular-expression-matching - submission-5

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n = len(s)
+        m = len(p)
+        dp=[[-1] * m for _ in range(n + 1)]
+        def sol(i,j):
+            if j >= m:
+                return i >= n
+            if dp[i][j] == -1:
+                cur_match = i < n and (s[i] == p[j] or p[j] == ".")
+                if j + 1 < m and p[j+1] == "*":
+                    if cur_match:
+                        dp[i][j] = sol(i + 1, j) or sol(i, j + 2)
+                    else:
+                        dp[i][j] = sol(i, j + 2)
+                elif cur_match:
+                    dp[i][j] = sol(i+1, j+1)
+                else:
+                    dp[i][j] = False
+            return dp[i][j]
+        return sol(0,0)
+
+        
